aabbe6f6b2a499611706ad3b407efd658e09dd4f
Juan Pablo Ante <juan.pablo.ante@correounivalle.edu.co>
2025-03-15 02:31:48 -0500

    Models and DB extraction done

--- views/presupuesto_view.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import (
-    QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel, QListWidget, QComboBox, QInputDialog
-)
-from controllers.presupuesto_controller import PresupuestoController
-
-class PresupuestoView(QWidget):
-    def __init__(self):
-        super().__init__()
